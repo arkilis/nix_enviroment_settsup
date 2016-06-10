b05d888db07453bef11e7f7b424704036f3991cd
Ben Liu <arkilis@gmail.com>
2016-06-10 00:23:17 +0000

    add mysql/apach2 for ubuntu server

---
+++ ubuntu14_setups.sh
@@ -0,0 +1,22 @@
+# update
+sudo apt-get update
+sudo apt-get upgrade
+
+# apach2 (php5 mod)
+sudo apt-get install apache2
+sudo apt-get install php5-mysql
+
+
+
+# mysql
+# which might ask for password
+sudo apt-get install mysql-server 
+
+# php5
+sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+
+
+# restart apache2
+sudo service apache2 restart
+
+
