9f649b4c5ea335b3cab499da630cad81a549f02a
Ben LIU <arkilis@gmail.com>
2016-08-05 05:28:59 +0000

    update ubuntu bash script

--- ubuntu14_setups.sh
+++ ubuntu14_setups.sh
@@ -15,6 +15,8 @@ sudo apt-get install libmysqlclient-dev
 
 # php5
 sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install php5-curl
+sudo apt-get install php5-gd
 
 
 # restart apache2
