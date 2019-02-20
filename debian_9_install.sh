8152707a2fa6de90e0189d467254f56781a90306
Ben LIU <arkilis@gmail.com>
2019-02-20 01:28:15 +0000

    update debian_9_install.sh

--- debian_9_install.sh
+++ debian_9_install.sh
@@ -3,6 +3,13 @@ sudo apt update
 sudo apt upgrade
 
 
+# install tools
+sudo apt-get install unzip
+sudo apt-get install curl
+sudo apt-get install -y software-properties-common
+
+
+
 # install python
 sudo apt-get install -y python-virtualenv
 
@@ -17,4 +24,10 @@ sudo apt-get install -y nginx
 
 # install php
 sudo apt-get install -y apt-transport-https lsb-release ca-certificates
+curl -fsSL https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+sudo apt-get install -y software-properties-common
+sudo apt update
+sudo apt install php7.2-common php7.2-cli
+# install nginx php
+sudo apt install php7.2-fpm
 
